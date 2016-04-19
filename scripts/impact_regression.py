8d7e3e6023dddcd4ed4dc1b48eaf185d0d37c7ae
LK <lenny.khazan@gmail.com>
2016-04-19 01:06:07 -0400

    Added data viz

--- scripts/impact_regression.py
+++ scripts/impact_regression.py
@@ -1,9 +1,10 @@
 # Imports
-import math
 import numpy as np
-import csv
 import load_data
 from linear_regression import LinearRegression
+from matplotlib.mlab import PCA
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 # Take an output vector y and apply a reverse log transform
 def reverse_transform(y):
@@ -20,7 +21,25 @@ def compute_cost(X, y, predictions):
 if __name__ == "__main__":
 	X, y = load_data.import_data()
 
-	model = LinearRegression()
-	model.fit(X, y)
-	
-	print compute_cost(X, y, model.predict(X))
+	# model = LinearRegression()
+	# model.fit(X, y)
+
+	res = PCA(X).Y
+	x_data = []
+	y_data = []
+	z_data = []
+
+	# y = reverse_transform(y)
+
+	for i in range(len(y)):
+		x_data.append(res[i, 0])
+		y_data.append(res[i, 1])
+		z_data.append(y[i])
+
+	fig1 = plt.figure() # Make a plotting figure
+	ax = Axes3D(fig1) # use the plotting figure to create a Axis3D object.
+	ax.scatter(x_data, y_data, z_data, 'bo')
+
+	ax.set_zlabel('Transformed area')
+
+	plt.show()
