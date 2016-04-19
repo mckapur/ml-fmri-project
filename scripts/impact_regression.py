16bd59e1ba8e59ceda318a53f794465dabbda5de
LK <lenny.khazan@gmail.com>
2016-04-18 22:49:32 -0400

    Partially fixed GD implementation

--- scripts/impact_regression.py
+++ scripts/impact_regression.py
@@ -2,9 +2,10 @@
 FOREST_FIRE_DATASET_PATH = './../datasets/forest-fire-damage/prepared_ff_damage_dataset.csv'
 STANDARDIZED_DATASET_DELIMETER = ','
 LEARNING_RATE = 1e-6
-MAX_EPOCHS = 1000
+MAX_EPOCHS = 10000
 
 # Imports
+import math
 import numpy as np
 import tensorflow as tf
 import csv
@@ -39,40 +40,56 @@ def prepare_data():
 
 # Learning
 
-# Declare the numerical variables for our weight(s), bias, and final output of the cost function
+# Declare the numerical variables for our weight(s) and bias
 W = 0
 b = 0
-final_cost = 0
 
-def linear_activation(W_tf, b_tf):
-	# Compute a linear activation with model: X*W + b
- 	return tf.add(tf.matmul(X, W_tf), b_tf)
-
-def learn(): # Train a model from data
-	# Declare the TensorFlow versions of the weight(s) and bias variables
+def learn(): # Train a model from data using TensorFlow
+	# Declare the TensorFlow versions of the weight(s) and bias variables and placeholders for input/output
+	X_tf = tf.placeholder("float32")
+	y_tf = tf.placeholder("float32")
 	W_tf = tf.Variable(tf.zeros([n, 1]), name="weight")
-	b_tf = tf.Variable(tf.constant(0.0, shape=[1, 1]), name="bias")
+	b_tf = tf.Variable(0.0, name="bias")
 
-	activation = linear_activation(W_tf, b_tf) # We will use a linear model
-	cost_function = tf.reduce_sum(tf.square(activation - y))/(2 * m) # Setup the cost function to be standardized L2 loss (mean squared error of activation vs. actual y)
+	activation = tf.add(tf.matmul(X_tf, W_tf), b_tf) # Compute a linear activation with model: X*W + b. This assumes X is an m x n matrix and W is an n x 1 vector (producing an m x 1 vector).
+	cost_function = tf.div(tf.reduce_sum(tf.square(tf.sub(activation, y_tf))), (2 * m)) # Setup the cost function to be standardized L2 loss (mean squared error of activation vs. actual y)
 	optimizer = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(cost_function) # Setup the cost optimization method as iterative gradient descent
 
 	with tf.Session() as sess: # Create the session
 		sess.run(tf.initialize_all_variables()) # Initialize the placeholder values
 
 		for i in range(MAX_EPOCHS): # Perform MAX_EPOCHS steps of optimization
-			sess.run(optimizer) # Perform a single weight update
+			sess.run(optimizer, feed_dict={X_tf: X, y_tf: y}) # Perform a single weight update
+
+			if i % 1000 == 0:
+				print 'Step #%d: %f' % (i, sess.run(cost_function, feed_dict={X_tf: X, y_tf: y}))
 
 		# Initialize our numerical weights from the TensorFlow session versions
-		global W, b, final_cost
+		global W, b
 		W = sess.run(W_tf)
 		b = sess.run(b_tf)
-		final_cost = sess.run(cost_function)
+
+# Prediction
+
+def predict(x): # Take a feature vector x and compute a prediction based on the weights
+	return math.exp(np.add(np.dot(np.transpose(W), x), b)) - 1 # First, we use the linear hypothesis function h(x) = x^Transpose*W + b, and then use that as input to the function g(x) = e^x - 1 as the inverse of the pre-processing ln(x + 1) step
+
+# Post-Training
+
+def compute_cost(): # Computes the universal cost over the training set
+	cost = 0
+	for i in range(X.shape[0]): # Iterate through all the training cases
+		cost += np.square(y[i] - predict(X[i, :])) # Get the square of discrepency between predicted and actual
+	cost /= (2 * m) # Get mean of cost
+	return cost
 
 def output_results(): # Output optimization results through the console
-	print 'Optimization complete with final cost', final_cost
-	print 'Final weight vector: \n', W
-	print 'Final bias value', b
+	print 'Optimization complete'
+	# print 'Final weight vector: \n', W
+	# print 'Final bias value', b
+	print 'Final cost is ', compute_cost()
+	# for i in range(X.shape[0]):
+	# 	print predict(X[i, :])
 
 if __name__ == "__main__":
 	import_data()
