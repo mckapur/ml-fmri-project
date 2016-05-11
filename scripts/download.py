886036d93a232615733d32693ac29c82c91e1542
LK <lenny.khazan@gmail.com>
2016-05-11 18:47:46 -0400

    Auto-resume download

--- scripts/download.py
+++ scripts/download.py
@@ -9,7 +9,11 @@ def download(data_folder='data/'):
         if filename == 'func_minimal':
             continue
 
-        key.get_contents_to_filename(os.path.join(data_folder, filename))
+        path = os.path.join(data_folder, filename)
+
+        if not os.path.isfile(path) or os.path.getsize(path) != key.size:
+            print 'Downloading %s (%.0fMB)' % (filename, key.size / 1024**2)
+            key.get_contents_to_filename(path)
 
 if __name__ == '__main__':
     download()
