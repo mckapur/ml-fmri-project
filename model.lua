5a3be36c806ecce84b357e47d0d91cb461023ab9
Rohan Kapur <me@rohankapur.com>
2016-05-16 21:30:52 +0800

    Just GRU

--- model.lua
+++ model.lua
@@ -2,24 +2,24 @@ function createModel(nGPU)
   local model = nn.Sequential()
 
   -- Split along the time dimension
-  model:add(nn.SplitTable(5))
+  model:add(nn.SplitTable(6))
 
-  -- Apply convolutions to each time slice
-  local c = nn.ConcatTable()
-  c:add(nn.VolumetricConvolution(1, 96, 5, 5, 5))
-  c:add(nn.ReLU(true))
-  c:add(nn.VolumetricMaxPooling(5, 5, 5, 5, 5, 5))
-  model:add(c)
+  -- -- Apply convolutions to each time slice
+  -- local c = nn.ConcatTable()
+  -- c:add(nn.VolumetricConvolution(1, 96, 5, 5, 5))
+  -- c:add(nn.ReLU(true))
+  -- c:add(nn.VolumetricMaxPooling(5, 5, 5, 5, 5, 5))
+  -- model:add(c)
 
   -- Build the recurrent layer
-  local r = nn.GRU(12*18*15, 10)
+  local r = nn.GRU(61*73*61, 100)
   model:add(nn.Sequencer(r))
 
   -- Only keep the last time-step's output
   model:add(nn.SelectTable(-1))
 
   -- Classify
-  model:add(nn.Linear(10, 2))
+  model:add(nn.Linear(100, 2))
   model:add(nn.LogSoftMax())
 
   -- https://github.com/soumith/imagenet-multiGPU.torch/blob/master/models/alexnet.lua
